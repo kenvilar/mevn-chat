7438c03b60d5b5d78bbcf9624e8b88239047ae49
Ken Vilar <kenvilar@gmail.com>
2018-08-14 23:30:26 +0800

    Continue working on AddRoom and ListofRoom components

--- src/components/AddRoom.vue
+++ src/components/AddRoom.vue
@@ -1,11 +1,56 @@
 <template>
-    
+    <b-row>
+        <b-col align-self="start">&nbsp;</b-col>
+        <b-col cols="6" align-self="center">
+            <h2>
+                Add Room
+                <b-link href="#/">(Room List)</b-link>
+            </h2>
+            <b-form @submit="addRoom">
+                <b-form-group id="fieldsetHorizontal"
+                              horizontal
+                              :label-cols="4"
+                              breakpoint="md"
+                              label="Enter Room Name">
+                    <b-form-input id="room_name" :state="state" v-model.trim="room.room_name"></b-form-input>
+                </b-form-group>
+                <b-button type="submit" variant="primary">Save</b-button>
+            </b-form>
+        </b-col>
+        <b-col align-self="end">&nbsp;</b-col>
+    </b-row>
 </template>
 
 <script>
-  export default {
-    name: 'add-room'
+import axios from 'axios'
+export default {
+  name: 'add-room',
+  data() {
+    return {
+      room: {}
+    }
+  },
+  methods: {
+    addRoom (e) {
+      e.preventDefault()
+      this.chat.room = this.$route.params.id
+      this.chat.message = this.chat.nickname + ' join the room'
+      axios.post('//localhost:3000/api/chat', this.chat)
+        .then((response) => {
+          this.$router.push({
+            name: 'ChatRoom',
+            params: {
+              id: this.$route.params.id,
+              nickname: response.data.nickname
+            }
+          })
+        })
+        .catch((err) => {
+          this.errors.push(err)
+        })
+    }
   }
+}
 </script>
 
 <style scoped>
--- src/components/RoomList.vue
+++ src/components/RoomList.vue
@@ -1,11 +1,68 @@
 <template>
-    
+    <b-row>
+        <b-col cols="12">
+            <h2>
+                Room List
+                <b-link href="#/add-room">(Add Room)</b-link>
+            </h2>
+            <b-table striped hover :items="rooms" :fields="fields">
+                <template slot="actions" scope="row">
+                    <b-btn size="sm" @click.stop="joinRoom(row._id)">Join</b-btn>
+                </template>
+            </b-table>
+            <ul v-if="errors && errors.length">
+                <li v-for="error of errors" :key="error">
+                    {{ error.message }}
+                </li>
+            </ul>
+        </b-col>
+    </b-row>
 </template>
 
 <script>
-  export default {
-    name: 'room-list'
+import axios from 'axios'
+
+export default {
+  name: 'room-list',
+  data () {
+    return {
+      fields: {
+        room_name: {
+          label: 'Room Name',
+          sortable: true,
+          'class': 'text-center'
+        },
+        created_date: {
+          label: 'Created Date',
+          sortable: true
+        },
+        actions: {
+          label: 'Action',
+          'class': 'text-center'
+        }
+      },
+      rooms: [],
+      errors: []
+    }
+  },
+  created () {
+    axios.get('//localhost:3000/api/room')
+      .then((res) => {
+        this.rooms = res.data
+      })
+      .catch((err) => {
+        this.errors.push(err)
+      })
+  },
+  methods: {
+    joinRoom (id) {
+      this.$router.push({
+        name: 'JoinRoom',
+        params: { id: id }
+      })
+    }
   }
+}
 </script>
 
 <style scoped>

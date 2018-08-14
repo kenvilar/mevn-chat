3646425598b1ae5b841c3593637a92941395cda2
Ken Vilar <kenvilar@gmail.com>
2018-08-14 23:48:44 +0800

    Run socket for listener and emitter

--- routes/chat.js
+++ routes/chat.js
@@ -1,7 +1,23 @@
 var express = require('express')
 var router = express.Router()
 var mongoose = require('mongoose')
+var app = express()
+var server = require('http').createServer(app)
+var io = require('socket.io')(server)
 var Chat = require('../models/Chat')
+
+server.listen(4000)
+
+io.on('connection', function (socket) {
+  console.log('User connected')
+  socket.on('disconnect', function () {
+    console.log('User disconnected')
+  })
+  socket.on('save-message', function (data) {
+    console.log(data)
+    io.emit('new-message', {message: data})
+  })
+})
 
 // Get
 router.get('/', function (req, res, next) {

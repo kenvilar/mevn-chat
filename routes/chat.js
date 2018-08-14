6605fa0987ff8766aa79b4ea845190abfe613201
Ken Vilar <kenvilar@gmail.com>
2018-08-14 22:40:01 +0800

    Chat controller

--- routes/chat.js
+++ routes/chat.js
@@ -1,11 +1,46 @@
 var express = require('express')
 var router = express.Router()
+var mongoose = require('mongoose')
+var Chat = require('../models/Chat')
 
-/*
- * Home Page
- * */
-router.get('/', function (req, res) {
-  res.send('Express RESTful API')
+// Get
+router.get('/', function (req, res, next) {
+  Chat.find(function (err, products) {
+    if (err) return next(err)
+    res.json(products)
+  })
+})
+
+// Show
+router.get('/:id', function (req, res, next) {
+  Chat.findById(req.params.id, function (err, chat) {
+    if (err) return next(err)
+    res.json(chat)
+  })
+})
+
+// Store
+router.post('/', function (req, res, next) {
+  Chat.create(req.body, function (err, chat) {
+    if (err) return next(err)
+    res.json(chat)
+  })
+})
+
+// Update
+router.put('/:id', function (req, res, next) {
+  Chat.findByIdAndUpdate(req.params.id, req.body, function (err, chat) {
+    if (err) return next(err)
+    res.json(chat)
+  })
+})
+
+// Delete
+router.delete('/:id', function (req, res, next) {
+  Chat.findByIdAndRemove(req.params.id, req.body, function (err, chat) {
+    if (err) return next(err)
+    res.json(chat)
+  })
 })
 
 module.exports = router

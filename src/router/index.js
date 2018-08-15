92bd0669681c8f064a09b9574ee4f9d4b68dbb52
Ken Vilar <kenvilar@gmail.com>
2018-08-15 09:28:06 +0800

    Revert

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
 import RoomList from '@/components/RoomList'
 import AddRoom from '@/components/AddRoom'
 import JoinRoom from '@/components/JoinRoom'
